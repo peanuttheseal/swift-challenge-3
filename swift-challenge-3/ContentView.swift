710716d3a3b8470c760831bedbaab3f1a89ebf4d
claire / seal <claire_lai_2028@mgs.sch.edu.sg>
2025-11-05 15:28:14 +0800

    Made the stopwatch! Found a working stopwatch tutorial, so this works now (ignore all the comments those are just information), but the main thing is that the stopwatch auto-resets if you start and stop and start again so we need to fix that

--- swift-challenge-3/ContentView.swift
+++ swift-challenge-3/ContentView.swift
@@ -7,11 +7,40 @@
 
 import SwiftUI
 struct ContentView : View {
-    var body: some View {
-        VStack {
-            Text("Hello, World!")
+    @State var startDate: Date? // Stores when the user presses the start button
+        @State var isAnimationPaused = true // Flag to control the animation update schedule
+        @State var lastUpdate: Date? // Stores the date of the last update on screen
+
+        var body: some View {
+            switch isAnimationPaused {
+            case true:
+                // By using a static `Date` value, the view will be static
+                // Using the `startDate` or Date.now fallback allows the stopwatch to pause and
+                // continue to display the duration
+                Text(Date.now, format: .stopwatch(startingAt: startDate ?? .now))
+            case false:
+                // Using a `TimeDataSource`, the view will animate
+                Text(TimeDataSource<Date>.currentDate, format: .stopwatch(startingAt: startDate ?? .now))
+            }
+            HStack {
+                Button("Start") {
+                    // Store the moment at which the user presses the Start button
+                    startDate = .now
+                    // Unpause the TimelineView's animation schedule
+                    isAnimationPaused = false
+                }
+                .buttonStyle(.bordered)
+                .tint(.green)
+                .disabled(isAnimationPaused == false)
+                Button("Stop") {
+                    // We only pause the animation schedule to stop the
+                    isAnimationPaused = true
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.red)
+                .disabled(isAnimationPaused)
+            }
         }
-    }
 }
 
 #Preview {
