f789e65191041ef231f8b25721b2e6dd996ee477
claire / seal <claire_lai_2028@mgs.sch.edu.sg>
2025-11-05 12:49:40 +0800

    Initial Commit

---
+++ swift-challenge-3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-challenge-3
+//
+//  Created by T Krobot on 5/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-challenge-3/swift_challenge_3App.swift
@@ -0,0 +1,17 @@
+//
+//  swift_challenge_3App.swift
+//  swift-challenge-3
+//
+//  Created by T Krobot on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct swift_challenge_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
