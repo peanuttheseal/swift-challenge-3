34df9ddbb4264dc8e0fb7ef514516f63c084a18e
claire / seal <claire_lai_2028@mgs.sch.edu.sg>
2025-11-05 15:30:18 +0800

    Removed all the comments so it's easier to read

--- swift-challenge-3/ContentView.swift
+++ swift-challenge-3/ContentView.swift
@@ -7,33 +7,26 @@
 
 import SwiftUI
 struct ContentView : View {
-    @State var startDate: Date? // Stores when the user presses the start button
-        @State var isAnimationPaused = true // Flag to control the animation update schedule
-        @State var lastUpdate: Date? // Stores the date of the last update on screen
+    @State var startDate: Date?
+        @State var isAnimationPaused = true
+        @State var lastUpdate: Date?
 
         var body: some View {
             switch isAnimationPaused {
             case true:
-                // By using a static `Date` value, the view will be static
-                // Using the `startDate` or Date.now fallback allows the stopwatch to pause and
-                // continue to display the duration
                 Text(Date.now, format: .stopwatch(startingAt: startDate ?? .now))
             case false:
-                // Using a `TimeDataSource`, the view will animate
                 Text(TimeDataSource<Date>.currentDate, format: .stopwatch(startingAt: startDate ?? .now))
             }
             HStack {
                 Button("Start") {
-                    // Store the moment at which the user presses the Start button
                     startDate = .now
-                    // Unpause the TimelineView's animation schedule
                     isAnimationPaused = false
                 }
                 .buttonStyle(.bordered)
                 .tint(.green)
                 .disabled(isAnimationPaused == false)
                 Button("Stop") {
-                    // We only pause the animation schedule to stop the
                     isAnimationPaused = true
                 }
                 .buttonStyle(.borderedProminent)
