860db4632975c0cf0fdab307ed3f6514a503e3cc
claire / seal <claire_lai_2028@mgs.sch.edu.sg>
2025-11-07 15:23:59 +0800

    Added a colour scheme code to the main .swift file that says IMPORTANT_INFO

---
+++ swift-challenge-3/IMPORTANT_INFO.swift
@@ -0,0 +1,25 @@
+//
+//  swift_challenge_3App.swift
+//  swift-challenge-3
+//
+//  Created by T Krobot on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct swift_challenge_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+// MAIN COLOUR SCHEME
+    // #FFF4D1 -> Main colour, super super pale yellow, use as a backgroumd
+    // #F5B678 -> Side colour, medium orange, use for the goals ring
+    // #F5C978 -> Side colour, light-medium orange
+    // #F5E778 -> Side colour, yellow tinted orange
+    // #F5D878 -> Side colour, yellow bordering on light orange
+    // #F1F578 -> Side colour, bright yellow bordering on neon, use as a last resort
