b26c352a85d2fa79e2a0a3f22f5e28ae48cecea5
Ying Hsin <limyinghsin@gmail.com>
2025-11-10 15:20:19 +0800

    set timer ( info never transfer over yet)

--- swift-challenge-3/SheetView.swift
+++ swift-challenge-3/SheetView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct SheetView: View {
     
         @Binding var isPresented: Bool
+    @State private var selectedHour = 00
+    let hours = [00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
+    @State private var selectedMinute = 00
+    let minutes = [00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59]
 
         var body: some View {
             VStack {
@@ -24,6 +28,36 @@ struct SheetView: View {
                         .padding()
                         .monospaced()
                     Spacer()
+                }
+            }
+            
+            Section{
+                HStack{
+                    VStack{
+                        Text("Hours")
+                            .font(.system(size:25))
+                            .bold()
+                        
+                        Picker("Hours", selection: $selectedHour)
+                        {
+                            ForEach (hours, id: \.self) {
+                                Text(String(format:"%02d", $0))
+                            }
+                        }
+                        .pickerStyle(.wheel)
+                    }
+                    VStack {
+                        Text("Minutes")
+                            .font(.system(size:25))
+                            .bold()
+                        
+                        Picker("Mintues", selection: $selectedMinute){
+                            ForEach(minutes, id: \.self){
+                                Text(String(format: "%02d", $0))
+                            }
+                        }
+                        .pickerStyle(.wheel)
+                    }
                 }
             }
         }
