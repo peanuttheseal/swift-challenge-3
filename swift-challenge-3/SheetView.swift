221d0ce089e9ebc66261d172bf8cd88b7fd965bd
Ying Hsin <limyinghsin@gmail.com>
2025-11-14 10:39:28 +0800

    selected time

--- swift-challenge-3/SheetView.swift
+++ swift-challenge-3/SheetView.swift
@@ -10,10 +10,15 @@ import SwiftUI
 struct SheetView: View {
     
         @Binding var isPresented: Bool
+    @Binding var goalTimeLeft: Int
     @State private var selectedHour = 00
     let hours = [00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
     @State private var selectedMinute = 00
     let minutes = [00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59]
+    
+    func selectedTime(){
+        goalTimeLeft = selectedHour * 60 + selectedMinute
+    }
 
         var body: some View {
             VStack {
@@ -59,6 +64,8 @@ struct SheetView: View {
                         .pickerStyle(.wheel)
                     }
                 }
+                
+                
             }
         }
     }
