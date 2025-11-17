9dcd4f8a2ff747f5f43f3955b9267850d86d7ccc
Sophie <lian_pei_ying_sophie_2028@mgs.sch.edu.sg>
2025-11-17 15:02:25 +0800

    fixed some stuff

--- swift-challenge-3/ContentView.swift
+++ swift-challenge-3/ContentView.swift
@@ -60,19 +60,16 @@ struct ContentView : View {
             VStack {
                 
                 // streak
-                Text("\(streak) days")
+                Text("🔥 \(streak) day streak")
                     .monospaced()
                 
                 let quotes = ["Let’s get studying!", "Don’t give up!", "You got this!", "Keep going!", "Come on, study today!", "I believe in you!", "You can do it!", "Believe in yourself!", "Don’t break your streak!", "Keep up the efforts!", "Study today!", "Carpe diem :)", "Seize the day!", "Get going!", "Please don’t kill me, study today!", "Start now!", "What are you waiting for?", "No sweat — study now!", "Study. Or else :)", "What a beautiful day!", "Keep up the hard work!", "Hello!", "Start studying now!", "I have faith in you!", "Get started now!", "Keep me alive! Study now!", "\(name) says you can do it!", "Hang in there!", "I’m proud of you!", "You’re doing a great job!", "Your hard work is paying off!", "Don’t worry, be happy!", "Nice work!", "Look how far you’ve come!", "\(name) says you’re doing great!", "It’d be a pity not to put in any work now…", "Keep up the awesome work!", "\(name) says do it for the brain work!", "Good evening!", "Exercise that mind, study today!", "Start studying today!", "Just a little bit of effort…", "You got this! Do it for me!", "Lock in!!", "A little bit of time makes a huge difference!", "Can you reach your goal today?", "You’re doing great!", "Make me proud!", "Don’t give up on studying!", "STUDY. NOW.", "Yay!", "It’s a great day!", "Isn't this wonderful?", "Have an amazing day!", "I’m watching!", "Make that effort!", "Come on, meet your goal!", "Good afternoon!", "Don’t waste away, study today!", "Why let your brain rot?", "Hey!", "Just a few minutes away from keeping your streak!", "You’ve come this far, it’d be a pity to stop now!", "You’re doing too well to stop now!", "You can do great things with just a few minutes of study time!", "Good day!", "Have a good day!", "Be productive — start studying today!", "Do get some studying in!", "Productivity is key!", "Good morning!"]
-                if let randomQuote = quotes.randomElement() {
-                    Text("\(randomQuote)")
-                        .onAppear {
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 20) {
-                                self.showContent = true
-                            }
-                            
-                        }
+                if let randomQuote = quotes.randomElement() {                    
+                    if goalTimeLeft - elapsedSeconds2 <= 0 {
+                        Text("\(randomQuote)")
+                    }
                 }
+                
                 
                 
                 // chicken animation
