6bdb34573ea58014d516a6a38f6d77144b874081
claire / seal <claire_lai_2028@mgs.sch.edu.sg>
2025-11-17 11:07:19 +0800

    removed a huge chunk of useless commented code

--- StreakWidget/StreakWidget.swift
+++ StreakWidget/StreakWidget.swift
@@ -32,32 +32,6 @@ struct Provider: TimelineProvider {
     }
 }
 
-
-//    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-//        let entry = SimpleEntry(date: Date(), streakCount: Int())
-//        completion(entry)
-//    }
-//
-//    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-//        var entries: [SimpleEntry] = []
-//
-//        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-//        let currentDate = Date()
-//        for hourOffset in 0 ..< 5 {
-//            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-//            let entry = SimpleEntry(date: entryDate, streakCount: )
-//            entries.append(entry)
-//        }
-//
-//        let timeline = Timeline(entries: entries, policy: .atEnd)
-//        completion(timeline)
-//    }
-
-//    func relevances() async -> WidgetRelevances<Void> {
-//        // Generate a list containing the contexts this widget is relevant in.
-//    }
-
-
 struct SimpleEntry: TimelineEntry {
     let date: Date
     let streakCount: Int
